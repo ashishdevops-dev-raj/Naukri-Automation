4caa0c2e1f58d9587767412a930615d08d1d13cd
ashishdevops-dev-raj <abhishekraj77001@gmail.com>
2025-11-29 17:15:10 +0530

    Add Keys import for Enter key submission method

--- login.py
+++ login.py
@@ -5,6 +5,7 @@ Handles secure login using environment variables
 
 import time
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
