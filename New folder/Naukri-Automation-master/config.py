d794d35b133af2ba73faef531bd0ed506914b0c5
Ashish Raj <ashishrajdevops@gmail.com>
2025-12-04 15:53:47 +0530

    Delete New folder/Naukri-Automation-master directory

--- New folder/Naukri-Automation-master/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    EMAIL = os.getenv("NAUKRI_EMAIL")
-    PASSWORD = os.getenv("NAUKRI_PASSWORD")
-    KEYWORDS = ["DevOps Engineer"]
-    LOCATION = "Bangalore"
-    APPLY_LIMIT = 5
